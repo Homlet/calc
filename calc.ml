32a95d3e6955253670bd2294a053e37d1c0f8192
Homletmoo <samlhub@gmail.com>
2016-09-19 20:47:44 +0100

    Separation of data (slr tables) and code.

--- calc.ml
+++ calc.ml
@@ -12,7 +12,7 @@ fun tl  Nil          = raise nil_tail
   | tl (Cons(x, xf)) = xf ();
 
 (* Lexer data types. *)
-datatype token_name = Int | Real | Mul | Add | Sub | Cos | Fact | End;
+datatype token_name = Add | Sub | Mul | Cos | Fact | Int | Real | End;
 datatype attr_value = IntAttr of int
                     | RealAttr of real
                     | NullAttr;
@@ -20,9 +20,7 @@ datatype token = Token of token_name * attr_value;
 
 (* Parser data types. *)
 type nonterminal_name = string;
-datatype symbol = NT of nonterminal_name
-                | T of token_name;
-datatype production = Production of nonterminal_name * symbol list;
+datatype production = Production of nonterminal_name * int;
 type state = int;
 datatype action = Shift of state
                 | Reduce of production
@@ -111,125 +109,113 @@ fun lex          Nil          = Nil
  * Note: by convention, the headmost element of a non-terminal's parse
  *       tree node's child list is it's leftmost constituent symbol. *)
 exception invalid_syntax;
+exception invalid_action_table;
+exception invalid_goto_table;
 exception internal_error;
 val parse =
   let
-    fun s i = Shift(i);
-    fun r l rs = Reduce(Production(l, rs));
-    fun action 0  Sub  = s 7
-      | action 0  Cos  = s 9
-      | action 0  Int  = s 10
-      | action 0  Real = s 11
-      | action 1  Add  = s 12
-      | action 1  Sub  = s 13
-      | action 1  End  = Accept
-      | action 2  Add  = r "expr" [NT("term")]
-      | action 2  Sub  =     action 2 Add
-      | action 2  End  =     action 2 Add
-      | action 3  Add  = r "term" [NT("factor")]
-      | action 3  Sub  =     action 3 Add
-      | action 3  Mul  = s 14
-      | action 3  End  =     action 3 Add
-      | action 4  Add  = r "factor" [NT("factorial")]
-      | action 4  Sub  =     action 4 Add
-      | action 4  Mul  =     action 4 Add
-      | action 4  Fact = s 15
-      | action 4  End  =     action 4 Add
-      | action 5  Add  = r "factorial" [NT("int")]
-      | action 5  Sub  =     action 5 Add
-      | action 5  Mul  =     action 5 Add
-      | action 5  Fact =     action 5 Add
-      | action 5  End  =     action 5 Add
-      | action 6  Add  = r "factor" [NT("real")]
-      | action 6  Sub  =     action 6 Add
-      | action 6  Mul  =     action 6 Add
-      | action 6  End  =     action 6 Add
-      | action 7  Int  = s 16
-      | action 7  Real = s 17
-    (* No state 8 because I made a mistake drawing up the table. *)
-      | action 9  Int  = s 10
-      | action 9  Real = s 11
-      | action 10 Add  = r "int" [T(Int)]
-      | action 10 Sub  =     action 10 Add
-      | action 10 Mul  =     action 10 Add
-      | action 10 Fact =     action 10 Add
-      | action 10 End  =     action 10 Add
-      | action 11 Add  = r "real" [T(Real)]
-      | action 11 Sub  =     action 11 Add
-      | action 11 Mul  =     action 11 Add
-      | action 11 End  =     action 11 Add
-      | action 12 Sub  = s 7
-      | action 12 Cos  = s 9
-      | action 12 Int  = s 10
-      | action 12 Real = s 11
-      | action 13 Sub  = s 7
-      | action 13 Cos  = s 9
-      | action 13 Int  = s 10
-      | action 13 Real = s 11
-      | action 14 Sub  = s 7
-      | action 14 Cos  = s 9
-      | action 14 Int  = s 10
-      | action 14 Real = s 11
-      | action 15 Add  = r "factorial" [NT("factorial"), T(Fact)]
-      | action 15 Sub  =     action 15 Add
-      | action 15 Mul  =     action 15 Add
-      | action 15 Fact =     action 15 Add
-      | action 15 End  =     action 15 Add
-      | action 16 Add  = r "int" [T(Sub), T(Int)]
-      | action 16 Sub  =     action 16 Add
-      | action 16 Mul  =     action 16 Add
-      | action 16 Fact =     action 16 Add
-      | action 16 End  =     action 16 Add
-      | action 17 Add  = r "real" [T(Real), T(Real)]
-      | action 17 Sub  =     action 17 Add
-      | action 17 Mul  =     action 17 Add
-      | action 17 Fact =     action 17 Add
-      | action 17 End  =     action 17 Add
-      | action 18 Add  = r "factor" [T(Cos), NT("factorial")]
-      | action 18 Sub  =     action 18 Add
-      | action 18 Mul  =     action 18 Add
-      | action 18 Fact = s 15
-      | action 18 End  =     action 18 Add
-      | action 19 Add  = r "factor" [T(Cos), NT("real")]
-      | action 19 Sub  =     action 19 Add
-      | action 19 Mul  =     action 19 Add
-      | action 19 End  =     action 19 Add
-      | action 20 Add  = r "expr" [NT("expr"), T(Add), NT("term")]
-      | action 20 Sub  =     action 20 Add
-      | action 20 End  =     action 20 Add
-      | action 21 Add  = r "expr" [NT("expr"), T(Sub), NT("term")]
-      | action 21 Sub  =     action 21 Add
-      | action 21 End  =     action 21 Add
-      | action 22 Add  = r "term" [NT("factor"), T(Mul), NT("term")]
-      | action 22 Sub  =     action 22 Add
-      | action 22 End  =     action 22 Add
-      | action _  _    = Error;
+    (* Load the action and goto functions from file. *)
+    local
+        (* Strip whitespace from the start and end of a string. *)
+        fun strip s =
+          let
+            val l = String.explode s;
+            fun left [] = []
+              | left (c::cs) = if Char.isSpace c then left cs
+                                                 else (c::cs);
+          in
+            String.implode (left (List.rev (left (List.rev l))))
+          end;
+        fun ordNT "expr"      = 0
+          | ordNT "term"      = 1
+          | ordNT "factor"    = 2
+          | ordNT "factorial" = 3
+          | ordNT "int"       = 4
+          | ordNT "real"      = 5
+          | ordNT _           = raise internal_error;
 
-    fun goto 0  "expr"      = 1
-      | goto 0  "term"      = 2
-      | goto 0  "factor"    = 3
-      | goto 0  "factorial" = 4
-      | goto 0  "int"       = 5
-      | goto 0  "real"      = 6
-      | goto 9  "factorial" = 18
-      | goto 9  "int"       = 5
-      | goto 9  "real"      = 19
-      | goto 12 "term"      = 20
-      | goto 12 "factor"    = 3
-      | goto 12 "factorial" = 4
-      | goto 12 "int"       = 5
-      | goto 12 "real"      = 6
-      | goto 13 "term"      = 21
-      | goto 13 "factor"    = 3
-      | goto 13 "factorial" = 4
-      | goto 13 "int"       = 5
-      | goto 13 "real"      = 6
-      | goto 14 "term"      = 22
-      | goto 14 "factor"    = 3
-      | goto 14 "factorial" = 4
-      | goto 14 "int"       = 5
-      | goto 14 "real"      = 6
-      | goto _  _           = ~1;
+        fun ordT Add  = 0
+          | ordT Sub  = 1
+          | ordT Mul  = 2
+          | ordT Cos  = 3
+          | ordT Fact = 4
+          | ordT Int  = 5
+          | ordT Real = 6
+          | ordT End  = 7;  (* End should always be ordered last... *)
+
+        (* Load a file formatted as a table to a list of row lists. *)
+        fun load path =
+          let
+            (* Open the file and read an int from the first line (# of rows). *)
+            val input = TextIO.openIn path;
+            val rows = valOf (Int.fromString (valOf (TextIO.inputLine input)));
+            val cols = valOf (Int.fromString (valOf (TextIO.inputLine input)));
+
+            (* Process a single line of fields: drop the first (line label),
+             * and strip whitespace from the following. Ensure there are `cols`
+             * number of fields in the returned line, by using options. *)
+            fun processLine () =
+              let
+                fun aux 0 l  _      = List.rev l
+                  | aux n l  []     = aux (n - 1) (NONE::l) []
+                  | aux n l (f::fs) = aux (n - 1) ((SOME(strip f))::l) fs;
+
+                val fs = String.fields (Char.contains ":,")
+                                       (valOf (TextIO.inputLine input));
+              in
+                aux cols [] (List.tl fs)
+              end;
+          in
+            List.tabulate (rows, fn i => processLine ())
+          end;
+
+        (* Process the goto table. *)
+        val gotoTable =
+          let
+            fun processCell  NONE     = ~1
+              | processCell (SOME(s)) =
+                  case Int.fromString s of (SOME(i)) => i
+                                         |  NONE     => ~1;
+            
+            val processRow = List.map processCell;
+          in
+            List.map processRow (load "goto.txt")
+              handle _ => raise invalid_goto_table
+          end;
+
+        (* Process the action table. *)
+        val actionTable =
+          let
+            fun processShift s =
+                Shift(valOf (Int.fromString (String.extract (s, 1, NONE))));
+            
+            fun processReduce s =
+              let
+                val fields = String.fields (Char.contains "\"") s;
+                val name = List.nth (fields, 1);
+                val symbols = valOf (Int.fromString (List.nth (fields, 2)));
+              in
+                Reduce(Production(name, symbols))
+              end;
+
+            fun processCell  NONE     = Error
+              | processCell (SOME(s)) =
+                  if String.isPrefix "a" s then Accept else
+                  if String.isPrefix "s" s then processShift s else
+                  if String.isPrefix "r" s then processReduce s
+                                           else Error;
+
+            val processRow = List.map processCell;
+          in
+            List.map processRow (load "action.txt")
+              handle _ => raise invalid_action_table
+          end;
+    in
+        fun goto state name =
+          List.nth (List.nth (gotoTable, state), ordNT name);
+        fun action state token =
+          List.nth (List.nth (actionTable, state), ordT token);
+    end;
 
     (* Push a new leaf (token) node to the forest. *)
     fun push t forest = (Lf(t))::forest;
@@ -239,7 +225,6 @@ val parse =
      * forest to form its children, and adding it to the head of the new forest. *)
     fun grow (Production(name, symbols)) forest =
       let
-        val n = List.length symbols; 
         val popN =
           let
             fun aux ys 0  _      = ys
@@ -249,17 +234,24 @@ val parse =
             aux []
           end;
       in
-        (Br(name, popN n forest))::(List.drop (forest, n))
+        (Br(name, popN symbols forest))::(List.drop (forest, symbols))
       end;
 
     (* Return the new state stack after reducing by a production. *)
     fun reduce (Production(name, symbols)) states =
       let
-        val base = List.drop (states, List.length symbols);
+        val base = List.drop (states, symbols);
       in
         (goto (List.hd base) name)::base
       end;
 
+    (* Implementation of the lr parsing algorithm.
+     *
+     * Note: `forest` is a list of sub-trees currently parsed, in ltr order.
+     *        To build a full parse tree: during a reduction, a number of
+     *        sub-trees determined by the reducing production are popped
+     *        from this list. They are then repurposed as the children of
+     *        a new sub-tree, which is pushed. *)
     fun lr _ [] _   = raise internal_error
       | lr _ _  Nil = raise invalid_syntax
       | lr forest (states as s::ss) (cons as Cons(t as Token(t_name, _), tf)) =
